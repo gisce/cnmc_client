727b51ec7dd0f8136ff47f6540488b34f8562e4f
Xavi Torelló <info@xaviertorello.cat>
2018-01-16 17:16:19 +0100

    Test CNMC.Client.fetch of CUPS

--- specs/client_spec.py
+++ specs/client_spec.py
@@ -22,6 +22,9 @@ config = {
 expected = {
 }
 
+LIST_OF_CUPS = ["ES0021000000228141PR", "ES0021000002097098PR", "ES0021000003589973DS"]
+LIST_OF_FILE_TYPES = ["SIPS2_PS_ELECTRICIDAD", "SIPS2_CONSUMOS_ELECTRICIDAD", "SIPS2_PS_GAS", "SIPS2_CONSUMOS_GAS"]
+
 with description('A new'):
     with before.each:
         with spec_VCR.use_cassette('init.yaml'):
@@ -41,7 +44,26 @@ with description('A new'):
                     response = self.client.list()
                     
                     assert response
-
+                    
+                    """
                     print (response)
                     for element in response.result:
                         print (element.nombre, element.estado, element.tipoFichero, element.uriDescargas)
+                    """
+
+        with context('reach files for some CUPS'):
+            with it('must be performed as expected'):
+                with spec_VCR.use_cassette('fetch.yaml'):
+                    the_cups = [ LIST_OF_CUPS[0] ]
+                    the_type = LIST_OF_FILE_TYPES[1]
+                    
+                    response = self.client.fetch(cups=the_cups, file_type=the_type)
+                    
+                    assert response
+                    print (response)
+                    
+                    """
+                    print (response)
+                    for element in response.result:
+                        print (element.nombre, element.estado, element.tipoFichero, element.uriDescargas)
+                    """

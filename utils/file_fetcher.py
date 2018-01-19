8f8623f92db5119542adf1d89baea34fb570a786
Xavi Torelló <info@xaviertorello.cat>
2018-01-19 15:00:47 +0100

    Add util file_fetcher.py
    Script that fetchs SIPS for a CUPS

---
+++ utils/file_fetcher.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import (absolute_import)
+
+from cnmc_client import Client
+
+config = {
+    'environment': 'prod'
+}
+
+LIST_OF_CUPS = ["ES0021000000228141PR", "ES0021000002097098PR", "ES0021000003589973DS"]
+LIST_OF_FILE_TYPES = ["SIPS2_PS_ELECTRICIDAD", "SIPS2_CONSUMOS_ELECTRICIDAD", "SIPS2_PS_GAS", "SIPS2_CONSUMOS_GAS"]
+
+client = Client(**config)
+
+def fetch_SIPS(cups, file_type=LIST_OF_FILE_TYPES[0], as_csv=False):
+    response = client.fetch(cups=cups, file_type=file_type, as_csv=as_csv)
+    if not response.error:
+        return response.result
+
+def list_available_files():
+    return client.list()
+
+# Get SIPS file as bytes
+SIPS_bytes = fetch_SIPS(cups=[LIST_OF_CUPS[0]])
+
+# Get SIPS file as CSV reader
+SIPS_csv = fetch_SIPS(cups=[LIST_OF_CUPS[0]], as_csv=True)
+
+for line in SIPS_csv:
+    print (', '.join(line))

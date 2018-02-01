d1062286c707f90bf18f30b35773a18ceb190a36
Xavi Torelló <info@xaviertorello.cat>
2018-02-01 11:46:13 +0100

    Improve tool to reach configuration arguments
    Use click library to handle passed parameters and arguments
    
    Also define a Util_CNCM class to envelop the tool

--- utils/sips_update_by_zip.py
+++ utils/sips_update_by_zip.py
@@ -5,59 +5,76 @@ import cnmc_client
 from pymongo import MongoClient
 from sets import Set
 
-config = {
-    'environment': 'prod',
-}
-
-mongo_config = {
-    'connection_url': 'localhost:27017',
-    'db': 'db',
-    'user': None,
-    'password': None,
-}
+import click
 
 LIST_OF_FILE_TYPES = ["SIPS2_PS_ELECTRICIDAD", "SIPS2_CONSUMOS_ELECTRICIDAD", "SIPS2_PS_GAS", "SIPS2_CONSUMOS_GAS"]
 
-def find_CUPS_by_zip(zipcode):
-    assert type(zipcode) == str and len(zipcode) == 5, "Provided zipcode '{}' is not valid".format(zipcode)
+class CNMC_Utils(object):
+    def __init__(self, cnmc_config, mongo_config):
+	self.client = cnmc_client.Client(**cnmc_config)
 
-    search_params = {
-        'codi_postal': zipcode,
+	# Initialize MongoDB collections
+	mongo = MongoClient("mongodb://" + mongo_config['connection_url'])
+	self.collections = {
+	    'cups': mongo[mongo_config['db']].giscedata_sips_ps,
+	    'consumptions': mongo[mongo_config['db']].giscedata_sips_consums,
+
+	    'destination_cups': mongo[mongo_config['db']].giscedata_sips_ps_fmtjul16,
+	    'destination_consumptions': mongo[mongo_config['db']].giscedata_sips_consums_fmtjul16,
+	}
+
+    def find_CUPS_by_zip(self, zipcode):
+	assert type(zipcode) in [str, unicode] and len(zipcode) == 5, "Provided zipcode '{}' is not valid".format(zipcode)
+
+	search_params = {
+	    'codi_postal': zipcode,
+	}
+
+	fields = {
+	    'name': 1,
+	}
+
+	cups = Set()
+	for a_cups in self.collections['cups'].find(search_params, fields):
+	    cups.add(a_cups['name'])
+
+	return cups
+
+    def fetch_SIPS(self, cups, file_type=LIST_OF_FILE_TYPES[0], as_csv=False):
+	response = self.client.fetch(cups=cups, file_type=file_type, as_csv=as_csv)
+	assert not response.error
+	return response.result
+
+
+@click.command()
+@click.option('--host', default='localhost', help='MongoDB host')
+@click.option('--port', default='27017', help='MongoDB port', type=click.INT)
+@click.option('--user', default=None, help='MongoDB user')
+@click.option('--password', default=None, help='MongoDB password')
+@click.option('--database', default='database', help='MongoDB database')
+@click.argument('zipcode', type=click.STRING)
+def main(zipcode, host, port, user, password, database):
+    cnmc_config = {
+	'environment': 'prod',
     }
 
-    fields = {
-        'name': 1,
+    mongo_config = {
+	'connection_url': "{}:{}".format(host, port),
+	'db': database,
+	'user': user,
+	'password': password,
     }
 
-    cups = Set()
-    for a_cups in collections['cups'].find(search_params, fields):
-        cups.add(a_cups['name'])
+    utils = CNMC_Utils(cnmc_config, mongo_config)
+    # Initialize CNMC Client
 
-    return cups
+    cups_list = utils.find_CUPS_by_zip(zipcode)
 
-def fetch_SIPS(cups, file_type=LIST_OF_FILE_TYPES[0], as_csv=False):
-    response = client.fetch(cups=cups, file_type=file_type, as_csv=as_csv)
-    assert not response.error
-    return response.result
+    SIPS_csv = utils.fetch_SIPS(cups=cups_list, as_csv=True)
+
+    for line in SIPS_csv:
+        print (line)
 
 
-# Initialize CNMC Client
-client = cnmc_client.Client(**config)
-
-# Initialize MongoDB collections
-mongo = MongoClient("mongodb://" + mongo_config['connection_url'])
-collections = {
-    'cups': mongo[mongo_config['db']].giscedata_sips_ps,
-    'consumptions': mongo[mongo_config['db']].giscedata_sips_consums,
-
-    'destination_cups': mongo[mongo_config['db']].giscedata_sips_ps_fmtjul16,
-    'destination_consumptions': mongo[mongo_config['db']].giscedata_sips_consums_fmtjul16,
-}
-
-cups_list = find_CUPS_by_zip("42100")
-
-SIPS_csv = fetch_SIPS(cups=cups_list, as_csv=True)
-
-for line in SIPS_csv:
-    print (line)
-
+if __name__ == '__main__':
+    main()

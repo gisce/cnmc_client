e7accba9ea98442d7449b1b4c95a6ddc74adb2bd
Eduard Carreras <ecarreras@gisce.net>
2021-09-30 09:37:04 +0200

    Bump to v0.4.1

--- cnmc_client/__init__.py
+++ cnmc_client/__init__.py
@@ -2,7 +2,7 @@
 
 from __future__ import (division, absolute_import, print_function, unicode_literals)
 
-__version__ = '0.4.0'
+__version__ = '0.4.1'
 
 from .client import Client
 from .cnmc import CNMC_API
